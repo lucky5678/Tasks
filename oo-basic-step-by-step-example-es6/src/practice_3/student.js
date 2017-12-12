76baa6790fb4c02793ca171ba2276bdd48caa187
lucky5678 <1712477640@qq.com>
2017-12-12 23:06:32 +0800

    116first

--- oo-basic-step-by-step-example-es6/src/practice_3/student.js
+++ oo-basic-step-by-step-example-es6/src/practice_3/student.js
@@ -1,1 +1,13 @@
+import Person from "../../src/practice_2/person.js";
+
+class student extends Person{
+    constructor(name,age,klass){
+        super(name,age);
+        this.klass=klass;
+    }
+    introduce(){
+        return `I am a Student. I am at Class ${this.klass}.`;
+    }
+}
+export default student
 
