53477a769d78df562df555d26d29d5141c222012
lucky5678 <1712477640@qq.com>
2017-12-21 21:10:00 +0800

    a

---
+++ task115/src/practice_5/student.js
@@ -0,0 +1,15 @@
+import Person from "../../src/practice_2/person.js";
+
+class student extends Person{
+    constructor(name,age,klass){
+        super(name,age);
+        this.klass=klass;
+    }
+    introduce(){
+        return `My name is ${this.name}. I am ${this.age} years old. I am a Student. I am at Class ${this.klass}.`;
+    }
+}
+export default student
+
+
+
